ea41f6721f7bfa0e16fd84e10561eb91bd5afb4a
yorimichi <yorimichi.dev@gmail.com>
2024-01-04 02:10:25 +0900

    feat: :zap: scrolltop button

---
+++ Client/components/layout/scrollButton/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const scrollTop = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+};
+</script>
+
+<template>
+  <VBtn
+    class="customizer-btn"
+    size="large"
+    icon
+    variant="flat"
+    color="secondary"
+    @click.stop="scrollTop"
+  >
+    <ArrowBigUpLineIcon />
+  </VBtn>
+</template>
