6215bacb325ecaa5a60a0371076759029b0ae9f1
yorimichi <yorimichi.dev@gmail.com>
2024-01-04 13:25:58 +0900

    fix: :bug: size fix

--- Client/components/page/form/index.vue
+++ Client/components/page/form/index.vue
@@ -17,73 +17,83 @@ const icon = BulbIcon;
 </script>
 
 <template>
-  <SharedUIParentCard :icon="icon" title="UI・機能などのアイディア募集中">
-    <div class="px-10">
-      <VCol cols="12">
-        <!-- User ID -->
-        <VLabel class="mb-2 font-weight-medium">ユーザーID</VLabel>
-        <VTextField
-          variant="outlined"
-          type="text"
-          placeholder="User ID"
-          color="primary"
-        >
-          <template v-slot:prepend-inner>
-            <UserIcon
-              stroke-width="1.5"
-              size="22"
-              class="text-medium-emphasis"
-            />
-          </template>
-        </VTextField>
+  <VRow class="justify-center ma-n2">
+    <VCol cols="12" md="9">
+      <VCard elevation="10">
+        <VCardItem>
+          <div class="mb-3">
+            <h3 class="text-h3 mb-2">問い合わせ</h3>
+            <div class="d-flex gap-2">
+              <BulbIcon width="20" />
+              <h5 class="text-body-1">見た目・機能についてアイディア募集中</h5>
+            </div>
+          </div>
 
-        <!-- Email -->
-        <VLabel class="mb-2 font-weight-medium">メールアドレス</VLabel>
-        <VTextField
-          variant="outlined"
-          type="email"
-          placeholder="Email"
-          color="primary"
-        >
-          <template v-slot:prepend-inner>
-            <MailIcon
-              stroke-width="1.5"
-              size="22"
-              class="text-medium-emphasis"
-            />
-          </template>
-        </VTextField>
+          <!-- User ID -->
+          <VLabel class="mb-2 font-weight-medium">ユーザーID</VLabel>
+          <VTextField
+            variant="outlined"
+            type="text"
+            placeholder="User ID"
+            color="primary"
+          >
+            <template v-slot:prepend-inner>
+              <UserIcon
+                stroke-width="1.5"
+                size="22"
+                class="text-medium-emphasis"
+              />
+            </template>
+          </VTextField>
 
-        <!-- Content -->
-        <VLabel class="mb-2 font-weight-medium">お問い合わせ項目</VLabel>
-        <v-select :items="formContents" label="Content">
-          <template v-slot:prepend-inner>
-            <Message2Icon
-              stroke-width="1.5"
-              size="22"
-              class="text-medium-emphasis"
-            />
-          </template>
-        </v-select>
+          <!-- Email -->
+          <VLabel class="mb-2 font-weight-medium">メールアドレス</VLabel>
+          <VTextField
+            variant="outlined"
+            type="email"
+            placeholder="Email"
+            color="primary"
+          >
+            <template v-slot:prepend-inner>
+              <MailIcon
+                stroke-width="1.5"
+                size="22"
+                class="text-medium-emphasis"
+              />
+            </template>
+          </VTextField>
 
-        <!-- Detail -->
-        <VLabel class="mb-2 font-weight-medium">お問い合わせ詳細</VLabel>
-        <VTextarea
-          variant="outlined"
-          type="email"
-          placeholder="..."
-          color="primary"
-        >
-        </VTextarea>
+          <!-- Content -->
+          <VLabel class="mb-2 font-weight-medium">お問い合わせ項目</VLabel>
+          <v-select :items="formContents" label="Content">
+            <template v-slot:prepend-inner>
+              <Message2Icon
+                stroke-width="1.5"
+                size="22"
+                class="text-medium-emphasis"
+              />
+            </template>
+          </v-select>
 
-        <!-- Submit -->
-        <div class="d-flex justify-end">
-          <VBtn color="primary" @click="clickSubmit" class="" flat>送信</VBtn>
-        </div>
+          <!-- Detail -->
+          <VLabel class="mb-2 font-weight-medium">お問い合わせ詳細</VLabel>
+          <VTextarea
+            variant="outlined"
+            type="email"
+            placeholder="..."
+            color="primary"
+          >
+          </VTextarea>
 
-        <!-- Feature -->
-        <div v-if="submitState">hello</div>
-      </VCol>
-    </div>
-  </SharedUIParentCard>
+          <!-- Submit -->
+          <div class="d-flex justify-end">
+            <VBtn color="primary" @click="clickSubmit" class="" flat>送信</VBtn>
+          </div>
+
+          <!-- Feature -->
+          <div v-if="submitState">hello</div>
+        </VCardItem>
+      </VCard>
+    </VCol>
+  </VRow>
 </template>
