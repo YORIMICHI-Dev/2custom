d4481901d83473964470799286d40af9214c66de
yorimichi <yorimichi.dev@gmail.com>
2024-01-18 13:35:09 +0900

    feat: :art: delete unnecessary block

--- Client/components/layout/header/Notification.vue
+++ Client/components/layout/header/Notification.vue
@@ -5,15 +5,15 @@ const notifications = ['test', 'test', 'test', 'test', 'test'];
   <!-- ---------------------------------------------- -->
   <!-- notifications DD -->
   <!-- ---------------------------------------------- -->
-  <v-menu :close-on-content-click="false">
+  <VMenu :close-on-content-click="false">
     <template v-slot:activator="{ props }">
-      <v-btn icon variant="text" color="primary" class="custom-hover-primary" v-bind="props">
-        <v-badge dot color="primary">
+      <VBtn icon variant="text" color="primary" class="custom-hover-primary" v-bind="props">
+        <VBadge dot color="primary">
           <BellRingingIcon stroke-width="1.5" size="22" />
-        </v-badge>
-      </v-btn>
+        </VBadge>
+      </VBtn>
     </template>
-    <v-sheet rounded="md" width="360" elevation="10">
+    <VSheet rounded="md" width="360" elevation="10">
       <div class="px-8 pb-4 pt-6">
         <div class="d-flex align-center justify-space-between">
           <h6 class="text-h5">お知らせ</h6>
@@ -26,12 +26,12 @@ const notifications = ['test', 'test', 'test', 'test', 'test'];
             <VListItemTitle>{{ item }}</VListItemTitle>
             <VListItemSubtitle>{{ item }}</VListItemSubtitle>
           </VListItem>
-          <v-divider></v-divider>
+          <VDivider></VDivider>
         </VList>
       </perfect-scrollbar>
       <div class="py-4 px-6 text-center">
-        <v-btn color="primary" variant="outlined" to="/notification" block>お知らせ・更新履歴</v-btn>
+        <VBtn color="primary" variant="outlined" to="/notification" block>お知らせ・更新履歴</VBtn>
       </div>
-    </v-sheet>
-  </v-menu>
+    </VSheet>
+  </VMenu>
 </template>
--- Client/components/layout/header/Profile.vue
+++ Client/components/layout/header/Profile.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import { profileItems } from './profileItem';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <VMenu :close-on-content-click="false">
@@ -28,24 +26,6 @@ import { profileItems } from './profileItem';
         </div>
         <VDivider />
       </div>
-      <!-- Route page -->
-      <VList class="py-0 theme-list" lines="two">
-        <VListItem v-for="item in profileItems" :key="item.title" class="py-4 px-8 custom-text-primary" :to="item.href">
-          <template v-slot:prepend>
-            <VAvatar size="48" color="lightprimary" class="mr-3" rounded="md">
-              <VImg :src="item.avatar" width="24" height="24" :alt="item.avatar" />
-            </VAvatar>
-          </template>
-          <div>
-            <h6 class="text-subtitle-1 font-weight-bold mb-2 custom-title">
-              {{ item.title }}
-            </h6>
-          </div>
-          <p class="text-subtitle-1 font-weight-regular textSecondary">
-            {{ item.subtitle }}
-          </p>
-        </VListItem>
-      </VList>
     </VSheet>
   </VMenu>
 </template>
